504b4044b59950c208565aa7f10d65373756911c
Ligia Stan <stan.ligia96@gmail.com>
2019-06-11 14:38:31 +0100

    Initial version

---
+++ src/test/scala/weeklyscala/MainSpec.scala
@@ -0,0 +1,12 @@
+package weeklyscala
+
+import weeklyscala.Main._
+import org.scalatest._
+
+class MainSpec extends FunSuite with Matchers {
+
+  test("add given two numbers then returns sum") {
+    add(2, 3) shouldBe 5
+  }
+}
+
